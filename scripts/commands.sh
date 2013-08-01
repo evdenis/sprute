f8df83035aa9930a24f81291deb33891a129e5a1
Denis Efremov <efremov@ispras.ru>
2013-08-01 23:26:30 +0400

    commands && deploy update

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -1,16 +1,33 @@
 #!/bin/bash -x
 
+DIR=/root/
 
 
-get_latest_stable_kernel () {
+compile_kernel () {
+   # link from main page
 	local LINK="http://www.kernel.org/$(wget -q -O - http://www.kernel.org | xmllint --recover --xpath '//*[@id="latest_link"]/a/@href' --html - 2>/dev/null | cut -d '"' -f 2)"
-	
-	wget $LINK
+   # kernel archive file name from link
+   local KFILE="$(echo $LINK | grep -o -e 'linux-.*$')"
+   # kernel source directory after unpacking
+   local KDIR="$(echo $KFILE | grep -o -e 'linux-[\.[:digit:]]\+[[:digit:]]')"
+   # latest(by installation time, not by version) available kernel config file;
+   # FIXME: by version
+   local KCONFIG=$(ls -1 -t /boot/config-* | head -n 1)
+
+   pushd $DIR
+
+   wget -q $LINK
+   tar xf $KFILE
+   cd $KDIR
+
+   cp $KCONFIG .config
+   #yes '' | make oldconfig > /dev/null
+   #make olddefconfig
+   make silentoldconfig
+
+   popd
 }
 
-useradd -G sudo,stapdev,stapusr,stapsys -m -s /bin/bash s2e
-cd /home/s2e/
-get_latest_stable_kernel &&
-tar xf linux*.tar.xf
+#useradd -G sudo,stapdev,stapusr,stapsys -m -s /bin/bash s2e
 
 
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@
 DIR=debian32
 
 mkdir -p $DIR
-sudo debootstrap --arch=i386 --include=systemtap,systemtap-client,systemtap-server,grub-pc,fakeroot,wget,ca-certificates,libxml2-utils,linux-image-686-pae,expect  --variant=buildd sid $DIR
+sudo debootstrap --arch=i386 --include=systemtap,systemtap-client,systemtap-server,grub-pc,fakeroot,wget,ca-certificates,libxml2-utils,ncurses-dev,linux-image-686-pae,expect  --variant=buildd sid $DIR
 
