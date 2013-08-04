d80ce50a924bf84b7143c081543483679a2c5414
Denis Efremov <yefremov.denis@gmail.com>
2013-08-04 16:13:21 +0400

    gcov: patch/unpatch scripts update

--- scripts/gcov/patch_makefile_for_gcov.sh
+++ scripts/gcov/patch_makefile_for_gcov.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
-cd /usr/src/linux/fs/
+# $1 - kernel sources directory
+# $2 - subdirectory for makefiles patch
 
-PATCHSTR="GCOV_PROFILE := y"
+patch_makefiles () {
+	local patchstr="GCOV_PROFILE := y"
 
-for i in $(find . -type f -name Makefile)
-do
-	grep -qFe "$PATCHSTR" "$i" || { sed -i "1i $PATCHSTR" "$i" > /dev/null 2>&1 && echo "SUCCESS: $i" || echo "FAIL: $i"; }
-done
+	for i in $(find . -type f -name Makefile)
+	do
+		grep -qFe "$patchstr" "$i" || { sed -i -e "1i $patchstr" "$i" > /dev/null 2>&1 && echo "SUCCESS: $i" || echo "FAIL: $i"; }
+	done
+}
+
+#FIXME: add to lib
+#check_kernel_sources_dir
+if [[ ! ( -d "$1" && -e "$1/Kbuild" ) ]]
+then
+	echo "Not in the kernel sources directory." 2>&1
+	exit 1
+fi
+
+if [[ -d "$2" ]]
+then
+	echo "Subdirectory should be set." 2>&1
+	exit 1
+fi
+
+pushd "${1}/${2}"
+	patch_makefiles
+popd
+
--- scripts/gcov/unpatch_makefile_from_gcov.sh
+++ scripts/gcov/unpatch_makefile_from_gcov.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-cd /usr/src/linux/fs/
+# $1 - kernel sources directory
+# $2 - subdirectory for makefiles patch
 
-#sed -i '1i GCOV_PROFILE := y\n' "$(find . -type f -name Makefile)"
-for i in $(find . -type f -name Makefile)
-do
-	sed -i '/GCOV_PROFILE := y/d' "$i"
-done
+unpatch_makefiles () {
+	local patchstr="GCOV_PROFILE := y"
+
+	#sed -i '1i GCOV_PROFILE := y\n' "$(find . -type f -name Makefile)"
+	for i in $(find . -type f -name Makefile)
+	do
+		sed -i -e "/${patchstr}/d" "$i"
+	done
+}
+
+#FIXME: add to lib
+#check_kernel_sources_dir
+if [[ ! ( -d "$1" && -e "$1/Kbuild" ) ]]
+then
+	echo "Not in the kernel sources directory." 2>&1
+	exit 1
+fi
+
+if [[ -d "$2" ]]
+then
+	echo "Subdirectory should be set." 2>&1
+	exit 1
+fi
+
+pushd "${1}/${2}"
+	unpatch_makefiles
+popd
 
