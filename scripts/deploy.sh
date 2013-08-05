4cb513c7697413dbc883cd9ed8740e2c204a7991
Denis Efremov <yefremov.denis@gmail.com>
2013-08-06 01:46:17 +0400

    deploy.sh upd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,51 @@
 #!/bin/bash -x
 
-dir=debian32
+ldir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-IFS=','
+source "${ldir}/lib/common.sh"
+
+
+dir=debian32
+scriptsd="${dir}/root/"
 
 packets=(systemtap systemtap-client systemtap-server)
-packets+=(wget ca-certificates)
 packets+=(grub-pc linux-image-686-pae)
 packets+=(kernel-package fakeroot ncurses-dev)
-packets+=(expect git libxml2-utils)
-
+packets+=(expect git libxml2-utils cron)
+packets+=(wget ca-certificates)
 #gcc-python-plugin deps python-devel 
 #packets+=(gcc-4.8-plugin-dev) script should install apropriate version of package
 packets+=(python-dev python-six python-pygments python-sphinx graphviz)
 
 
-mkdir -p $dir
-sudo debootstrap --arch=i386 --include="${packets[*]}"  --variant=buildd sid $dir
+
+deploy_debian () {
+	local -i ret
+
+	IFS=','
+	mkdir -p "$dir"
+	debootstrap --arch=i386 --include="${packets[*]}"  --variant=buildd sid "$dir"
+	ret=$?
+	unset IFS
+	return $ret
+}
+
+copy_sprute () {
+	true
+}
+
+copy_scripts () {
+	cp -fr "$ldir" "$scriptsd"
+}
+
+copy_files () {
+	copy_scripts &&
+	copy_sprute
+}
+
+check_root
+
+deploy_debian &&
+copy_files "$dir" &&
+./chroot.sh "$dir" "${scriptsd}/inchroot.sh"
 
