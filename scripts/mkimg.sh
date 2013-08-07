c73084cccf8b5cc935ba592bd7219c24d86f22bd
Denis Efremov <efremov@ispras.ru>
2013-08-07 19:20:48 +0400

    mking update

--- scripts/mkimg.sh
+++ scripts/mkimg.sh
@@ -1,9 +1,16 @@
 #!/bin/bash -x
 
+user="work"
 name="test.raw"
 size="20G"
 system="./debian32/"
 mountpoint="/mnt/s2e_test_img/"
+mbr="/usr/share/syslinux/mbr.bin"
+
+ldir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "${ldir}/lib/common.sh"
+
 
 # we can use partx to notify kernel about new partition on loop device
 
@@ -11,11 +18,13 @@ mountpoint="/mnt/s2e_test_img/"
 # $2 - img size
 create_raw_img () {
    truncate -s "$2" "$1"
+   chown "${user}:${user}" "$1"
 }
 
 # TODO: param to spec filsystem on currently hidden part
 # $1 - img name
 partition_img () {
+   file_exists "$1" &&
    parted "$1" -s -- mklabel msdos &&
    parted "$1" -s -- mkpart primary ext2 1MiB 10GiB set 1 boot on &&
    parted "$1" -s -- mkpart primary linux-swap 10GiB 12GiB &&
@@ -27,65 +36,73 @@ loopdev=''
 # $1 - img name
 # $2 - mountpoint
 mount_img () {
+   if ! file_exists "$1"
+   then
+      return 1
+   fi
+
    mkdir -p "$2"
    loopdev=$(losetup -P --find --show "$1") &&
    mkfs.ext2 "${loopdev}p1" &&
+   mkswap "${loopdev}p2" &&
    mount "${loopdev}p1" "$2"
 }
 
+# $1 - orig
+# $2 - dest
 copy_root () {
-   rsync -rpa "${system}/" "$mountpoint"
+   dir_exists "$1" &&
+   dir_exists "$2" &&
+
+   rsync -rpa "${1}/" "$2"
 }
 
-install_grub () {
-#   grub2-install --boot-directory="${mountpoint}/boot/" --modules="ext2 part_msdos" "$loopdev"
-http://superuser.com/questions/130955/how-to-install-grub-into-an-img-file
+# $1 - device
+# $2 - mountpoint
+# $3 - mbr file
+install_extlinux () {
+   if [[ -e "$1" ]] && dir_exists "$2" && file_exists "$3"
+   then
+      #local -i size=$(stat -c '%s' "$3") &&
+      #extlinux --install "${2}/boot/" &&
+      #dd bs=$size conv=notrunc count=1 if="$3" of="$1" &&
 
-
-modprobe dm_mod
-kpartx -va /root/rootfs.img # *.img is setup elsewhere
-# normally you now would mount /dev/loop0p1 directly. BUT
-# grub specialists didn't manage to work with loop partitions other than /dev/loop[0-9]
-losetup -v -f --show /dev/mapper/loop0p1
-mount /dev/loop1 /mnt
-mkdir -p /mnt/boot/grub
-
-# change into chrooted environment. all remaining work will be done from here. this differs from the howto above.
-LANG=C chroot /mnt /bin/bash
-set -o vi
-mount -t sysfs sysfs /sys
-mount -t proc  proc  /proc
-# avoid grub asking questions
-cat << ! | debconf-set-selections -v
-grub2   grub2/linux_cmdline                select   
-grub2   grub2/linux_cmdline_default        select   
-grub-pc grub-pc/install_devices_empty      select yes
-grub-pc grub-pc/install_devices            select   
-!
-apt-get -y install grub-pc
-# don't setup device.map prior to this point. It will be overwritten by grub-pc install
-cat > /mnt/boot/grub/device.map << !
-(hd0)   /dev/loop0
-(hd0,1) /dev/loop1
-!
-# install here to fill /boot/grub for grub-mkconfig (update-grub)
-grub-install /dev/loop0
-# generate /boot/grub/grub.cfg
-update-grub
-
+      "${ldir}/chroot.sh" "$2" extlinux-install "$1" \&\& extlinux-update
+   else
+      return 1
+   fi
 }
 
 # $1 - mountpoint
 update_fstab () {
-   local fstab="${1}/etc/fstab"
-   grep -qFe '/dev/sda1' "$fstab" || { echo '/dev/sda1 / ext2 defaults 1 1'>> "$fstab";}
-   grep -qFe '/dev/sda2' "$fstab" || { echo '/dev/sda2 swap swap defaults 0 0'>> "$fstab";}
+   if dir_exists "$1"
+   then
+      local fstab="${1}/etc/fstab"
+      grep -qFe '/dev/sda1' "$fstab" || { echo '/dev/sda1 / ext2 defaults 1 1'>> "$fstab";}
+      grep -qFe '/dev/sda2' "$fstab" || { echo '/dev/sda2 swap swap defaults 0 0'>> "$fstab";}
+   else
+      return 1
+   fi
 }
 
+# $1 - mountpoint
+setup_root_autologin () {
+   dir_exists "$1" &&
+   sed -i -e 's#1:2345:respawn:/sbin/getty 38400 tty1#& --autologin root#' "${1}/etc/inittab"
+}
+
+# $1 - bootstrap 
+# $2 - mountpoint
+# $3 - mbr file
 deploy_system () {
-   copy_root &&
-   update_fstab &&
-   install_grub
+   dir_exists "$1" &&
+   dir_exists "$2" &&
+   file_exists "$3" &&
+
+   copy_root "$1" "$2" &&
+   update_fstab "$2" &&
+   setup_root_autologin "$2" &&
+   install_extlinux "$loopdev" "$2" "$3"
 }
 
 umount_img () {
@@ -93,14 +110,12 @@ umount_img () {
    losetup -d "$loopdev"
 }
 
-#check_root
+check_root
 
 create_raw_img "$name" "$size" &&
 partition_img  "$name" &&
 mount_img "$name" "$mountpoint" &&
-trap "unmount_img" HUP INT QUIT TERM &&
-deploy_system "$mountpoint"
+trap "umount_img" HUP INT QUIT TERM &&
+deploy_system "$system" "$mountpoint" "$mbr"
 umount_img
 
-
-#losetup -D
