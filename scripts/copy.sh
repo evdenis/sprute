17a6f9e9a4a207c857a656483987bc4940b5e642
Denis Efremov <efremov@ispras.ru>
2013-08-09 19:29:18 +0400

    copy script

---
+++ scripts/copy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -x
+
+ldir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "${ldir}/lib/common.sh"
+
+load_default_config || exit 1
+
+
+git ls-tree -r HEAD --name-only .
+
+copy_sprute () {
+   mkdir -p "$sprute_dir"
+   pushd "${ldir}/../"
+      # --exclude='scripts/' doesn't work  
+      rsync -vRau $(git ls-tree -r HEAD --name-only . | grep -vFe 'scripts/') "$sprute_dir"
+   popd
+}
+
+copy_scripts () {
+   mkdir -p "$scripts_dir"
+   pushd "$ldir"
+      rsync -vRau $(git ls-tree -r HEAD --name-only .) "./data/vm_img.conf" "$scripts_dir"
+   popd
+}
+
+copy_files () {
+	copy_scripts &&
+	copy_sprute
+}
+
+check_root
+
+copy_files
+
