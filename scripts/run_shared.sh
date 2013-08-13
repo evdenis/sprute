e574915d09837871fb823a72e56c8fdcfac5e71e
Denis Efremov <efremov@ispras.ru>
2013-08-13 19:03:36 +0400

    run_shared update

--- scripts/run_shared.sh
+++ scripts/run_shared.sh
@@ -1,7 +1,16 @@
 #!/bin/bash -x
 
+ldir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "${ldir}/lib/common.sh"
+
+load_default_config || exit 1
+
+mkdir -p "$host_shared_folder"
+
+# daemonize
 qemu-kvm -enable-kvm -fsdev \
-local,id=tag1,path="$HOME/vm_shared",security_model=none \
+local,id=tag1,path="$host_shared_folder",security_model=none \
 -device virtio-9p-pci,fsdev=tag1,mount_tag=binaries \
--hda ./test.raw
+-hda "$mkimg_name"
 
