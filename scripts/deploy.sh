cfd3bf58cd9c230f5e0c9a76698561c37d8f0ec4
Denis Efremov <efremov@ispras.ru>
2013-08-14 19:25:01 +0400

    bc in deps

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ load_default_config || exit 1
 packets=(systemtap systemtap-client systemtap-server)
 packets+=(extlinux linux-image-686-pae)
 packets+=(kernel-package fakeroot libncurses5-dev)
-packets+=(expect git libxml2-utils cron ssh liblockfile-bin)
+packets+=(expect git libxml2-utils cron ssh liblockfile-bin bc)
 packets+=(wget ca-certificates)
 #gcc-python-plugin deps python-devel 
 #packets+=(gcc-4.8-plugin-dev) script should install apropriate version of package
