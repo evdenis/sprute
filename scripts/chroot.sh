dfb289861b5f749d54d352533779506c1228dedd
Denis Efremov <efremov@ispras.ru>
2013-08-29 14:19:42 +0400

    chroot.sh: ability to work with raw images

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -4,11 +4,35 @@ ldir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "${ldir}/lib/common.sh"
 
+load_default_config || exit 1
 
-chroot_path="$1"
+path="$1"
 shift
 
+loop_mount=''
+
+if [[ -d "$path" ]]
+then
+   chroot_path="$path"
+   loop_mount='n'
+elif [[ -f "$path" ]]
+then
+   loop_mount='y'
+else
+   echo "Path to chroot folder or raw image should be provided." 1>&2
+   exit 1
+fi
+
+loopdev=''
+
 pre_chroot_mount () {
+   if [[ "$loop_mount" == 'y' ]]
+   then
+      chroot_path="${default_dir:-$(mktemp -d)}" &&
+      mkdir -p $chroot_path &&
+      loopdev=$(losetup -P --find --show "$path") &&
+      mount "${loopdev}p1" "$chroot_path" || return 1
+   fi
    mount -t proc none "$chroot_path/proc" &&
    mount --rbind /dev "$chroot_path/dev"  &&
    mount -t sysfs sys "$chroot_path/sys"
@@ -18,6 +42,14 @@ post_chroot_umount () {
    umount "$chroot_path/proc"
    umount "$chroot_path/sys"
    umount --lazy "$chroot_path/dev"
+   if [[ "$loop_mount" == 'y' ]]
+   then
+      sync
+      umount "${loopdev}p1" &&
+      sleep 3s &&
+      losetup -d "$loopdev" &&
+      rmdir "$chroot_path"
+   fi
 }
 
 exec_cmd () {
@@ -26,7 +58,7 @@ exec_cmd () {
 
 run () {
    pre_chroot_mount &&
-   trap post_chroot_umount HUP INT QUIT TERM
+   trap post_chroot_umount HUP INT QUIT TERM &&
    exec_cmd "$@"
    post_chroot_umount
 }
