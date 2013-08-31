cc5cc641232ccc0cd1908a6efa275c3741d935aa
Denis Efremov <yefremov.denis@gmail.com>
2013-08-31 16:57:09 +0400

    upd

--- get_structure/generate_vfs_concolic_structures.sh
+++ get_structure/generate_vfs_concolic_structures.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -x
 
-rm -f vfslib.stp
+output="../staplib/vfslib.stpm"
+
+rm -f "$output"
 
 for i in inode file dentry super_block
 do
-   ./get_structure.pl --path /home/work/workspace/linux/include/ --name "$i" -m >> vfslib.stp
-   echo >> vfslib.stp
+   ./get_structure.pl --path /home/work/workspace/linux/include/ --name "$i" -m >> "$output"
+   echo >> "$output"
 done
 
