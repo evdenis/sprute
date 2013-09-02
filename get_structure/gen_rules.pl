5d5ee5c286ad50e7cf61ea5ee820573ea9d1bf3f
Denis Efremov <efremov@ispras.ru>
2013-09-02 16:55:57 +0400

    gen_rules.pl: module name; fix for duplicating functions

--- get_structure/gen_rules.pl
+++ get_structure/gen_rules.pl
@@ -6,16 +6,42 @@ use 5.10.0;
 use feature qw(say);
 
 use File::Slurp qw(read_file);
+use List::MoreUtils qw(uniq);
+use Getopt::Long qw(:config gnu_getopt);
 
-#TODO: module name
 #TODO: arg names
-#TODO: duplicating functions
 #TODO: out of the module functions
 #TODO: module dependence
 
-my $module = "fat";
+my $path = '';
+my $depdb = '';
+my $cbdir = '';
+my $module = '';
 
-foreach my $i (<>) {
+GetOptions(
+   'path|p=s' => \$path,
+   'depdb|d=s' => \$depdb,
+   'cbdir|c=s' => \$cbdir,
+   'module|m=s' => \$module,
+) or die "Incorrect usage!\n";
+
+die "Path, depdb, cbdir and module should be set.\n" if ( ! ( $path and $module and $depdb and $cbdir ) );
+die "${path} - is not a path to kernel sources dir." if (! -f "${path}/Kbuild");
+die "Can't read ${depdb}" if (! -r $depdb);
+die "There is no such directory ${cbdir}" if (! -d $cbdir);
+die "Can't read ${module} in ${cbdir}" if (! -r "${cbdir}/${module}.sprute" );
+
+my @modules_files = read_file($depdb, chomp => 1);
+
+my @str = grep(/\/${module}.ko :=/, @modules_files);
+
+die "Wrong format of ${depdb}: number of ${module}.ko occurences $#str." if ( $#str ne 0 );
+
+my @operations = read_file("${cbdir}/${module}.sprute", chomp => 1);
+
+@operations = uniq(@operations);
+
+foreach my $i (@operations) {
    $i =~ m/^(?<st>\w+);(?<op>\w+)=(?<cb>\w+)$/;
    my ($struct, $callback, $function) = ($+{st}, $+{op}, $+{cb});
 
