bda442d11d45f904f779a4674fed995d4fc3e1c5
Denis Efremov <efremov@ispras.ru>
2013-09-12 19:40:28 +0400

    run-s2e.sh: moving fix

--- scripts/run-s2e.sh
+++ scripts/run-s2e.sh
@@ -10,10 +10,13 @@ case "$1" in
       shift;;
 esac
 
+typeset -i init_timestamp=$(stat --printf='%X' ./s2e-last/)
+
 log="$(mktemp)"
 errlog="$(mktemp)"
 
-trap "{ mv '$log' ./s2e-last/run.log; mv '$errlog' ./s2e-last/err.log; }" EXIT
+trap "{ if (( $init_timestamp < \$(stat --printf='%X' ./s2e-last/) )); then mv '$log' ./s2e-last/run.log; mv '$errlog' ./s2e-last/err.log; else rm -f '$log' '$errlog'; fi; }" EXIT
+sleep 1s
 
 "${S2EDIR}/build/qemu-${mode}/x86_64-s2e-softmmu/qemu-system-x86_64" -serial stdio "$@" > >( tee "$log" ) 2> >( tee "$errlog" >&2 )
 
