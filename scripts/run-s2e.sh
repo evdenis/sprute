3f42e2ea6982c6e6fc8b6ecb8fc7fcc89a2cd48e
Denis Efremov <efremov@ispras.ru>
2013-09-12 17:17:08 +0400

    run-s2e.sh: upd

--- scripts/run-s2e.sh
+++ scripts/run-s2e.sh
@@ -1,4 +1,19 @@
 #!/bin/bash -x
 
-"$S2EDIR/build/qemu-release/x86_64-s2e-softmmu/qemu-system-x86_64" -serial stdio "$@"
+mode="release"
+
+case "$1" in
+   "release")
+      shift;;
+   "debug")
+      mode="debug";
+      shift;;
+esac
+
+log="$(mktemp)"
+errlog="$(mktemp)"
+
+trap "{ mv '$log' ./s2e-last/run.log; mv '$errlog' ./s2e-last/err.log; }" EXIT
+
+"${S2EDIR}/build/qemu-${mode}/x86_64-s2e-softmmu/qemu-system-x86_64" -serial stdio "$@" > >( tee "$log" ) 2> >( tee "$errlog" >&2 )
 
